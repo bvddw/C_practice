d7d8cadfe5bcbd869ba550786a0f09f74e6ac0fe
Arsenii <122690212+bvddw@users.noreply.github.com>
2023-05-24 12:51:50 +0300

    practice university

--- task_4.c
+++ task_4.c
@@ -12,7 +12,13 @@ int main()
     if ((a >= b + c) || (b >= a + c) || (c >= a + b))
         printf("A triangle with such sides does not exist.\n");
     else
+    {
         printf("A triangle with such sides exists.\n");
+        if ((a * a == b * b + c * c) || (b * b == a * a + c * c) || (c * c == a * a + b * b))
+            printf("The triangle is rectangular.\n");
+        else
+            printf("The triangle is not rectangular.\n");
+    }
     system("pause");
     return 0;
 }
